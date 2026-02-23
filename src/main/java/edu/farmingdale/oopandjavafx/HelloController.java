76cf2e28c334896e3c81163dcc77cad974ac95c9
Murid Quorishi <fazilq22@gmail.com>
2026-02-23 17:29:08 -0500

    Add MenuBar with File and Edit menus and connect basic event handlers

--- src/main/java/edu/farmingdale/oopandjavafx/HelloController.java
+++ src/main/java/edu/farmingdale/oopandjavafx/HelloController.java
@@ -1,4 +1,21 @@
 package edu.farmingdale.oopandjavafx;
 
+import javafx.fxml.FXML;
+
 public class HelloController {
+
+    @FXML
+    private void handleNew() {
+        System.out.println("New clicked");
+    }
+
+    @FXML
+    private void handleDelete() {
+        System.out.println("Delete clicked");
+    }
+
+    @FXML
+    private void handleExit() {
+        System.exit(0);
+    }
 }
