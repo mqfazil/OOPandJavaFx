e1aba1798c8fb2af60759457baa3a7afe8381b4e
Murid Quorishi <fazilq22@gmail.com>
2026-02-23 21:34:27 -0500

    Apply CSS styling to MenuBar and TableView components

--- src/main/java/edu/farmingdale/oopandjavafx/HelloApplication.java
+++ src/main/java/edu/farmingdale/oopandjavafx/HelloApplication.java
@@ -11,8 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        var css = HelloApplication.class.getResource("style.css");
+        if (css != null) {
+            scene.getStylesheets().add(css.toExternalForm());
+        }
+        stage.setTitle("JavaFX Menu & Table Application");
         stage.setScene(scene);
         stage.show();
     }
