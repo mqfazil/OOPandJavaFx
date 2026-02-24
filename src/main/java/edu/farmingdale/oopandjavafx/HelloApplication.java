bce59be8c52de95e27dca22e0922fbd81bfb5d19
Murid Quorishi <fazilq22@gmail.com>
2026-02-23 21:53:25 -0500

    Change window size

--- src/main/java/edu/farmingdale/oopandjavafx/HelloApplication.java
+++ src/main/java/edu/farmingdale/oopandjavafx/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
         var css = HelloApplication.class.getResource("style.css");
         if (css != null) {
             scene.getStylesheets().add(css.toExternalForm());
